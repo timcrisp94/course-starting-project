91bd0454b456109e2b475760f8078b48606ba2d7
Tim Crisp <timothycrisp@gmail.com>
2022-05-03 12:18:05 -0500

    add enum def and js

--- app.ts
+++ app.ts
@@ -1,4 +1,34 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Tim',
+//   age: 35,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// }
+
+// person.role[1] = 10;
+
+// for (const hobby of person.hobbies) {
+//   console.log(hobby.toUpperCase());
+// }
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
 const person = {
   name: 'Tim',
-  age: 35
+  age: 35,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN
+};
+
+if (person.role === Role.ADMIN) {
+  console.log('is author')
 }
