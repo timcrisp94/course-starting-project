2421048143a75c1db54c34b5bb32efd6181e1cdd
Tim Crisp <timothycrisp@gmail.com>
2022-05-03 12:30:28 -0500

    add union types

--- app.ts
+++ app.ts
@@ -1,34 +1,14 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: 'Tim',
-//   age: 35,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, 'author']
-// }
-
-// person.role[1] = 10;
-
-// for (const hobby of person.hobbies) {
-//   console.log(hobby.toUpperCase());
-// }
-
-enum Role { ADMIN, READ_ONLY, AUTHOR };
-
-const person = {
-  name: 'Tim',
-  age: 35,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN
-};
-
-if (person.role === Role.ADMIN) {
-  console.log('is author')
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString;
+  }  
+  return result;
 }
+
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('Hank', 'Anna');
