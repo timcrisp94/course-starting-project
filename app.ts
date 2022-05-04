2ac006251c5d925761aaded1faad90ecaf3af560
Tim Crisp <timothycrisp@gmail.com>
2022-05-04 09:57:51 -0500

    callbacks

--- app.ts
+++ app.ts
@@ -6,4 +6,23 @@ function printResult(num: number): void {
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
+
+// function types describe which type of function
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+
+
+console.log(combineValues(8, 8))
+
+addAndHandle(10, 20, (result) => {
+  console.log(result)
+})
