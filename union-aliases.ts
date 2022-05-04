df5a9cc3bfb79b3a9ed4e84176f3453156dafa25
Tim Crisp <timothycrisp@gmail.com>
2022-05-04 07:31:46 -0500

    implement union aliases

---
+++ union-aliases.ts
@@ -0,0 +1,30 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text'
+
+function combine(
+  input1: Combinable, 
+  input2: Combinable, 
+  resultType: ConversionDescriptor
+  ) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  // if(resultType === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+
+  // } 
+}
+
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combine(30, 26, 'as-number');
+console.log(combinedStringAges); 
+
+const combinedNames = combine('Hank', 'Anna', 'as-text');
+console.log(combinedNames)
